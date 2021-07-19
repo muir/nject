cbc67cb4556c4140081c258515936fbcffed957e
David Sharnoff <muir@idiom.org>
2021-07-18 21:58:49 -0700

    WIP, nvelope

--- nvelope/deferred.go
+++ nvelope/deferred.go
@@ -2,35 +2,42 @@ package nvelope
 
 import (
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
-
-type deferredWriter struct {
-	base http.ResponseWriter
-	passthough bool
-	header http.Header
-	buffer []byte
-	status int
+// DeferredWriter that wraps an underlying http.ResponseWriter.
+// DeferredWriter buffers writes and headers.  The buffer can be
+// reset.  When it's time to actually write, use Flush().
+type DeferredWriter struct {
+	base        http.ResponseWriter
+	passthough  bool
+	header      http.Header
+	buffer      []byte
+	status      int
 	resetHeader http.Header
 }
 
-func NewDeferredWriter(w http.ResponseWrite) {
-	return &deferredWriter{
-		base: w,
-		header: w.Header().Clone(),
+func NewDeferredWriter(w http.ResponseWrite) *DeferredWriter {
+	return &DeferredWriter{
+		base:        w,
+		header:      w.Header().Clone(),
 		resetHeader: w.Header().Clone(),
-		buffer = make([]byte, 0, 4*1024),
+		buffer:      make([]byte, 0, 4*1024),
 	}
 }
 
-func (w *deferredWriter) Header() http.Header {
+// Header is the same as http.ResponseWriter.Header
+func (w *DeferredWriter) Header() http.Header {
 	if w.passthrough {
 		return w.base
 	}
 	return w.header
 }
 
-func (w *deferredWriter) Write(b []byte) (int, error) {
+// Write is the same as http.ResponseWriter.Write
+// except that the action is delayed until Flush() is called.
+func (w *DeferredWriter) Write(b []byte) (int, error) {
 	if w.passthrough {
 		return w.base.Write(b)
 	}
@@ -38,7 +45,9 @@ func (w *deferredWriter) Write(b []byte) (int, error) {
 	return len(b), nil
 }
 
-func (w *deferredWriter) WriteHeader(statusCode int) {
+// WriteHeader is the same as http.ResponseWriter.WriteHeader
+// except that the action is delayed until Flush() is called.
+func (w *DeferredWriter) WriteHeader(statusCode int) {
 	if w.passthrough {
 		w.base.WriteHeader(statusCode)
 	} else {
@@ -46,15 +55,83 @@ func (w *deferredWriter) WriteHeader(statusCode int) {
 	}
 }
 
-func (w *deferredWriter) Reset() {
+// Reset empties the DeferredWriter's buffers and resets its Header
+// back to its original state.  Reset returns error if UnderlyingWriter()
+// or Flush() have been called.
+func (w *DeferredWriter) Reset() error {
+	if w.passthrough {
+		return errors.New("Attempt to reset a DeferredWriter after it is in passthrough mode")
+	}
 	w.buffer = nil
 	w.status = 0
 	w.header = w.resetHeader.Clone()
+	return nil
 }
 
-func (w *deferredHeader) PreserveHeader() {
+// PreserveHeader saves the current Header so that a Reset will revert
+// back to the header just saved.
+func (w *DeferredWriter) PreserveHeader() {
 	w.resetHeader = w.header.Clone()
 }
 
-func (w *deferredHeader) Send() error {
+// UnderlyingWriter returns the underlying writer.  Any header
+// modifications made with the DeferredWriter are copied to the
+// base writer.  After a call to UnderlyingWriter, the DeferredWriter
+// switches to passthrough mode: all future calls to Write(),
+// Header(), etc are passed through to the http.ResponseWriter that
+// was used to initialize the DeferredWrited.
+func (w *deferredWrite) UnderlyingWriter() http.ResponseWriter {
+	w.passthrough = true
+	h := w.base.Header()
+	for k := range h {
+		if v, ok := w.header[k]; ok {
+			h[k] = v
+		} else {
+			delete(h, k)
+		}
+	}
+	for k, v := range w.header {
+		if _, ok := h[k]; ok {
+			continue
+		}
+		h[k] = v
+	}
+	return w.base
+}
+
+// Flush pushes the buffered write content through to the base writer.
+// You can only flush once.  After a flush, all further calls are passed
+// through to be base writer.  WriteHeader() will be called on the base
+// writer even if there is no buffered data.
+func (w *DeferredWriter) Flush() error {
+	if w.passthrough {
+		return errors.New("Attempt flush deferred writer that is not deferred")
+	}
+	base := w.UnderlyingWriter()
+	if w.status != 0 {
+		base.WriteHeader(w.status)
+	}
+	for i := 0; i < len(w.buffer)-1; {
+		amt, err := base.Write(w.buffer[i:])
+		if err != nil {
+			// Is this handling of short writes necessary?  Perhaps
+			// so since a followup write will probably give a
+			// more accurate error.
+			if err == io.ErrShortWrite {
+				i += amt
+				continue
+			}
+			return errors.Wrap(err, "flush buffered writer")
+		}
+		return nil
+	}
+}
+
+// FlushIfNotFlushed calls Flush if the DeferredWriter is not in
+// passthrough mode.
+func (w *DeferredWriter) FlushIfNotFlushed() error {
+	if !w.passthrough {
+		return w.Flush()
+	}
+	return nil
 }
