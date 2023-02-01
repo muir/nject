7b070496e25929fffd8bb0b54e2ba77cc98d30b8
David Muir Sharnoff <github@dave.sharnoff.org>
2023-01-31 22:02:45 -0800

    add warning about duplicated types to DetailedError

--- error.go
+++ error.go
@@ -2,6 +2,7 @@ package nject
 
 import (
 	"errors"
+	"sync"
 )
 
 type njectError struct {
@@ -20,7 +21,44 @@ func (ne *njectError) Error() string {
 func DetailedError(err error) string {
 	var njectError *njectError
 	if errors.As(err, &njectError) {
+		dups := duplicateTypes()
+		if dups != "" {
+			return err.Error() + "\n\n" + njectError.details +
+				"\n\nWarning: the following type names refer to more than one type:\n" +
+				dups
+		}
 		return err.Error() + "\n\n" + njectError.details
 	}
 	return err.Error()
+}
+
+var duplicatesThrough int
+var dupLock sync.Mutex
+var duplicates string
+var duplicatesFound = make(map[string]struct{})
+
+func duplicateTypes() string {
+	max := func() int {
+		lock.Lock()
+		defer lock.Unlock()
+		return typeCounter
+	}()
+	dupLock.Lock()
+	defer dupLock.Unlock()
+	if duplicatesThrough == max {
+		return duplicates
+	}
+	names := make(map[string]struct{})
+	for i := 1; i <= typeCounter; i++ {
+		n := typeCode(i).String()
+		if _, ok := names[n]; ok {
+			if _, ok := duplicatesFound[n]; !ok {
+				duplicates += " " + n
+				duplicatesFound[n] = struct{}{}
+			}
+		}
+		names[n] = struct{}{}
+	}
+	duplicatesThrough = max
+	return duplicates
 }
